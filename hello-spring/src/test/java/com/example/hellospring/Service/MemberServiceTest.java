3a0497eee0609e058b3f8c0de1b84e23b2d5e098
nznotg <nznotg@gmail.com>
2022-07-20 23:05:36 +0900

    MemberService Test method add : validateDuplicateMember & BeforeEach, AfterEach

--- hello-spring/src/test/java/com/example/hellospring/Service/MemberServiceTest.java
+++ hello-spring/src/test/java/com/example/hellospring/Service/MemberServiceTest.java
@@ -1,7 +1,10 @@
 package com.example.hellospring.Service;
 
 import com.example.hellospring.domain.Member;
+import com.example.hellospring.repository.MemoryMemberRepository;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
@@ -10,13 +13,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
     //
-    MemberService memberService = new MemberService();
+    MemberService memberService;
+    MemoryMemberRepository repository;
+
+    @BeforeEach
+    void beforeEach() {
+        repository = new MemoryMemberRepository();
+        memberService = new MemberService(repository);
+    }
+
+    @AfterEach
+    void afterEach() {
+        repository.clearStore();
+    }
+
 
     @Test
     void 회원가입() {
         //given
         Member member = new Member();
-        member.setName("choi");
+        member.setName("Kim");
 
         //when
         Long saveId = memberService.join(member);
@@ -24,6 +40,29 @@ class MemberServiceTest {
         //then
         Member foundMember = memberService.findOne(saveId).get();
         Assertions.assertThat(member.getName()).isEqualTo(foundMember.getName());
+    }
+
+    @Test
+    void 중복_회원_예외() {
+        //given
+        Member member1 = new Member();
+        member1.setName("Kim");
+
+        Member member2 = new Member();
+        member2.setName("Kim");
+
+        //when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        Assertions.assertThat(e.getMessage()).isEqualTo("this member name already exists.");
+        //        try {
+//            memberService.join(member2);
+//            fail("예외 처리가 되어야 합니다.");
+//        } catch (IllegalStateException e) {
+//            Assertions.assertThat(e.getMessage()).isEqualTo("this member name already exists.");
+//        }
+        //then
     }
 
     @Test
